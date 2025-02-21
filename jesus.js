8a198a845cfb1bfd254e71935a328af5d722334f
jesusreinaldoj <150970673+jesusreinaldoj@users.noreply.github.com>
2025-02-22 00:28:22 +0100

    Archivos separados de la pagina de Jesus Reinaldo

---
+++ jesus.js
@@ -0,0 +1,10 @@
+let boton=document.getElementById("boton");
+let divJesus=document.getElementById("jesus");
+
+boton.onclick=clickboton;
+
+function clickboton(){
+
+    divJesus.style.backgroundColor="red";
+
+}
